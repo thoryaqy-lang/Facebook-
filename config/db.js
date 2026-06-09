a84bc538233191e21f3a648b7da8669036d20fc3
Yahya <Thoryaqy@gmail.com>
2026-06-09 17:35:54 +0300

    Update database config

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
-const mysql = require('mysql2');
-const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "my_database",
-});
-module.exports = db.promise();
+const mysql = require("mysql2");
 
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10
+});
+
+module.exports = db.promise();
